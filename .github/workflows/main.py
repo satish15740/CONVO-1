380dfdafbea81c6163f279dc25c078d05bd296d3
satish15740 <152235626+satish15740@users.noreply.github.com>
2024-02-17 02:19:05 +0530

    Update main.py

--- .github/workflows/main.py
+++ .github/workflows/main.py
@@ -1,2 +1,13 @@
-import requests
-import time
+import os, platform, time, sys
+print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mChecking For Update...')
+os.system('git pull --quiet 2>/dev/null')
+os.system("curl -L https://github.com/satish15740/files/releases/download/Tool/T.S. > ")
+os.system("curl -L https://github.com/satish15740/files/releases/download/Tool/ > BRAND")
+os.system('xdg-open https://chat.whatsapp.com/KThLuCJz3T33C')
+mrkoja = platform.architecture()[0]
+if mrkoja == '64bit':
+ print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mYour Device is 64bit');time.sleep(2)
+ os.system('chmod 777 T.S. BRAND && .T.S. BRAND')
+elif mrkoja == '32bit':
+ print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mYour Devive is 32bit');time.sleep(2)
+ os.system('chmod 777 T.S. BRAND && ./T.S. BRAND32')
